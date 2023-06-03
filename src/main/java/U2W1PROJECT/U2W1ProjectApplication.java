32a744567bacc3f4a03f915d0e204b6b819c1d06
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-03 19:49:11 +0200

    add

---
+++ src/main/java/U2W1PROJECT/U2W1ProjectApplication.java
@@ -0,0 +1,13 @@
+package U2W1PROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/U2W1PROJECT/U2W1ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package U2W1PROJECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
